b74ec790041a2c187bd9272c11d5edd950d113ad
Wanda Gleason <oglissane@gmail.com>
2021-02-14 23:44:50 -0500

    Adding portfolio files html, css, js etc

---
+++ Assets/JS/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#my-btn").hover(function(){
+        $(this).toggleClass("btn-danger");
+    });
+  
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+         x.className += " responsive";
+        } 
+        else {
+            x.className = "topnav";
+            }
+    }
+});
