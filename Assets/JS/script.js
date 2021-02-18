55aff1ab2a91a8d321f88b65eb6fb2162f27ccba
Wanda Gleason <oglissane@gmail.com>
2021-02-17 21:47:37 -0500

    Still working on slide effect

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,33 +1,32 @@
-
-
 $(document).ready(function(){
-    $('#portfolio .thumbnail').hover(function(){
-      $(this).children(".cust-caption").slideDown();
-    },
-    function(){
-      $(this).children(".cust-caption").fadeOut();
-    });
-  
-      
-    // see and scroll menu 
-    $("#navbar a.areas").click( function(e) {
-    
-      // prevent default anchor click behavior
-      e.preventDefault();
-  
-      // store areas
-      var areas = this.areas;
-  
-      // Destination
-      var scrollOffset = $(this.areas).offset().top;
-  
-      // animate
-      $('html, body').animate({
-        scrollTop: scrollOffset
-      }, 500, function(){
-        window.location.areas = areas;
-      }) 
-    })
+  $('#portfolio.thumbnail').hover(function(){
+    $(this).children(".cust-caption").slideDown();
+  },
+  function(){
+    $(this).children(".cust-caption").fadeOut();
   });
+
+    
+  // spy and scroll menu boogey
+  $("#navbar a.hash").click( function(e) {
   
+    // prevent default anchor click behavior
+    e.preventDefault();
+
+    // store hash
+    var areas = this.areas;
+
+    // Destination
+    var scrollOffset = $(this.areas).offset().top;
+
+    // animate
+    $('html, body').animate({
+      scrollTop: scrollOffset
+    }, 500, function(){
+      window.location.areas = areas;
+    }) 
+  })
+});
+
+
 
